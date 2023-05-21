dacea6222f88ec446a73035d4a1879a9f4b1b2c3
Heber-739 <Heberduarteryr@gmail.com>
2023-05-21 18:35:42 -0300

    header getUser

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/backend/service/user.service';
+import { CRUDLocalService } from 'src/app/backend/service/CRUD-Local.service';
+import { AuthService } from 'src/app/backend/service/auth.service';
 import { DataUser } from 'src/app/interface/dataUser';
 
 @Component({
@@ -10,13 +11,22 @@ import { DataUser } from 'src/app/interface/dataUser';
 export class HeaderComponent implements OnInit {
   edithMode: boolean = false;
   user: DataUser = {} as DataUser;
-  constructor(private userService: UserService) {}
-
+  constructor(private authS: AuthService, private local: CRUDLocalService) {
+    this.getUser();
+  }
   ngOnInit(): void {
-    this.userService.subscribeUser().subscribe({
-      next: (res) => {
-        this.user = res;
-      },
+    this.authS.edith$().subscribe({
+      next: (res) => (this.edithMode = res),
     });
+    this.authS.logged$().subscribe({ next: () => this.getUser() });
+  }
+
+  getUser() {
+    const getuser = setInterval(() => {
+      this.user = this.local.getUserData('user');
+      if (this.user != null) {
+        clearInterval(getuser);
+      }
+    }, 1000);
   }
 }
