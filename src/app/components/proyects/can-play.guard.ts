b5d81e9e46dbfd432ff240592c1e3e4c7bfa3e9b
Heber Duarte <Heberduarteryr@gmail.com>
2024-03-05 06:51:26 -0300

    update angular to 16.2.12

--- src/app/components/proyects/can-play.guard.ts
+++ src/app/components/proyects/can-play.guard.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CanPlayGuard implements CanActivate {
+export class CanPlayGuard  {
   constructor(private router: Router) {}
   canActivate():
     | boolean
